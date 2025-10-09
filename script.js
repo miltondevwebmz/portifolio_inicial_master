cf390dbbd5ad4e02877d3c51e130d9c880642e45
miltonfrontcode <miltonamonecumba@gmail.com>
2025-10-09 09:54:59 +0200

    Primeiro commit do portfólio
    Adicionando arquivos iniciais do meu portifolio inicial com html, css, js, fontes, biblioteca awesome.

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+// Mudar tema
+function toggle() {
+    document.body.classList.toggle("light");
+}
+
+// abrir e fechar menu
+
+
+// abrir menu automaticamente em telas grandes e fechar automaticamente em telas pequenas
+
