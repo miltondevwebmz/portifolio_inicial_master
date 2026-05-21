63425f10c6c38313f30c3e2b3210e5189aaf9ce5
miltondevwebmz <miltonamonecumba@gmail.com>
2026-05-21 16:18:04 +0200

    Modifiquei os bugs de layout e link

--- script.js
+++ script.js
@@ -4,10 +4,7 @@ function toggle() {
     document.body.classList.toggle("light");
 }
 
-
-
 // abrir e fechar menu
 
 
 // abrir menu automaticamente em telas grandes e fechar automaticamente em telas pequenas
-
