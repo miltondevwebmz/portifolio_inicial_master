222111a043838f5a8d665270a7e72045d5fbba56
miltondevwebmz <miltonamonecumba@gmail.com>
2026-05-23 15:46:26 +0200

    links de contacto, botao ver mais

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 
 // Mudar tema
 function toggle() {
-    document.body.classList.toggle("light");
+    document.body.classList.toggle("dark");
 }
 
 // abrir e fechar menu
 
 
 // abrir menu automaticamente em telas grandes e fechar automaticamente em telas pequenas
+
+const btn = document.getElementById('verBtn')
+const cardsEscondidos = document.querySelectorAll('.cards:nth-child(n+3)')
+let expandido = false
+
+btn.addEventListener('click', () => {
+    expandido = !expandido;
+
+    cardsEscondidos.forEach(card => {
+        card.classList.toggle('mostrar', expandido)
+    })
+
+    btn.textContent = expandido ? 'Ver menos' : 'Ver mais';
+})
